2e364ffaad92730ffe37255fa0aeda6acf08519a
Albert Johansson <albert.j.jo@gmail.com>
2018-11-24 14:23:37 +0100

    Removed duplicate print

--- edvard problem_1.py
+++ edvard problem_1.py
@@ -6,7 +6,7 @@ Created on Sat Nov 24 12:57:35 2018
 """
 
 sum_of_numbers = 0 
-print("Now, sum of all numbers is " + str(sum_of_numbers))
+
 for i in [1,2,3,4,5,6,7,8,9,10,11,12]:
     print("Now, i is " + str(i))
     print("Now, sum of all numbers is " + str(sum_of_numbers))
